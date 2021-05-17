68b4ad3a597af82af23c63ae5f190011e0982d03
mrsyafapri <rizkisyafapri@gmail.com>
2021-05-17 17:53:36 +0700

    first commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,14 @@
+// Navbar
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function () {
+    nav.classList.toggle('slide');
+});
+
+// Footer
+const date = new Date();
+const year = date.getFullYear();
+
+const footerYear = document.getElementById('footer-year')
+footerYear.innerHTML = year;
